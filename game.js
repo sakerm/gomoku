bc06b3cec7125c3a2b94ec619f670703ae1a78bc
Lorenzo Meresse <lorenzomeresse@Lorenzos-MacBook-Pro.local>
2019-07-04 13:29:18 +0200

    voila

--- game.js
+++ game.js
@@ -1,15 +1,3 @@
-var lastClicked;
-var grid = clickableGrid(19,19,function(el,row,col){
-    console.log("You clicked on element:",el);
-    console.log("You clicked on row:",row);
-    console.log("You clicked on col:",col);
-
-    el.className='clicked';
-    timerStart = true;
-});
-
-document.body.appendChild(grid);
-
 //counter
 var boxCounter = 0.0001;
 var c = document.getElementById("myCanvas");
@@ -283,33 +271,5 @@ function scoring() {
     //text
 }
 //end scoring
-
-
-//button restart and others
-
-function clickableGrid( rows, cols, callback ){
-    var grid = document.createElement('table');
-    grid.className = 'grid';
-   
-    for (var r=0;r<rows;++r){
-        var tr = grid.appendChild(document.createElement('tr'));
-        for (var c=0;c<cols;++c){
-            var cell = tr.appendChild(document.createElement('td'));
-            cell.addEventListener('click',(function(el,r,c){
-                return function() {
-                    //ne peux pas poser plusieurs pieces aux meme endroit
-                    if (el.className == "clicked")
-                    {
-                        alert("you can not put a token");
-                        return ;
-                    }
-                    callback(el,r,c);
-                }
-            })
-            (cell,r,c),false);
-        }
-    }
-    return grid;
-}
 
 scoring();
---
+++ game_playable.js
@@ -0,0 +1,82 @@
+function	game() {
+
+var board = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var current_player = 1;
+var	ia = false;
+var	game_canvas = document.getElementById("game");
+var	x;
+var y;
+var	playing = true;
+var	ctx = game_canvas.getContext("2d");
+var w = game_canvas.width;
+var h = game_canvas.height;
+
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect();
+    y = event.clientY - rect.top;
+    x = event.clientX - rect.left;
+    x = Math.trunc(x / (w/19));
+    y = Math.trunc(y / (h/19));
+}
+
+function drawCanvas()
+{
+	for (var i = 0; i < 18; i++)
+	{
+		ctx.beginPath();
+		ctx.moveTo(w/19 + i*(w/19), 0);
+		ctx.lineTo(w/19 + i*(w/19), h);
+		ctx.moveTo(0, h/19 + i*(h/19));
+		ctx.lineTo(w, h/19 + i*(h/19));
+		ctx.stroke();
+	}
+}
+
+function click()
+{
+	ctx.beginPath();
+	ctx.arc(x*(w/19)+(w/19/2), y*(h/19)+(h/19/2), w/19/2.3, 0, 2*Math.PI);
+	if (current_player == 1)
+		ctx.fillStyle = "blue";
+	else
+		ctx.fillStyle = "red";
+	ctx.fill();
+	ctx.stroke();
+	board[x][y] = current_player;
+	if (current_player == 1)
+		current_player = 2;
+	else
+		current_player = 1;
+}
+
+drawCanvas();
+game_canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(game_canvas, e);
+    if ((current_player == 1 || ia == false) && playing == true && board[x][y] == 0)
+    	click();
+});
+
+}
+
+game();

03eefdfa396720764f9398a53b624926f5e3888a
Lorenzo Meresse <lorenzomeresse@lorenzos-macbook-pro.home>
2019-08-02 13:10:11 +0200

    new

--- heuristique.js
+++ heuristique.js
@@ -1,9 +1,107 @@
 var	nbHeuristique = 0;
 
+function checkNbPieces(i, j, player)
+{
+	var nbpieces = 0;
+	var space = 0;
+	var counti = 0;
+	var countj = 0;
+
+	while ((i+counti > 0 && i+counti <= 18 && j+countj >= 0 && j+countj <= 18) && counti > -3 || (board[i+counti][j] == player && i+counti > 0))
+		counti--;
+	while((i+counti >= 0 && i+counti <= 18 && j+countj >= 0 && j+countj <= 18))
+	{
+		if (board[i+counti][j] == player)
+		{
+			nbpieces++;
+			if (nbpieces == 1) 
+				space = 0;
+		}
+		else if (board[i+counti][j] == 0)
+			space++;
+		else if (space == 2 || (board[i+counti][j] != player && board[i+counti][j] != 0))
+			break;
+		counti++;
+	}
+
+	space = 0;
+	counti = 0;
+	countj = 0;
+
+	while ((i+counti > 0 && i+counti <= 18 && j+countj >= 0 && j+countj <= 18) && counti > -3 || (board[i+counti][j] == player && i+counti > 0))
+		countj--;
+	while((i+counti >= 0 && i+counti <= 18 && j+countj >= 0 && j+countj <= 18))
+	{
+		if (board[i+counti][j+countj] == player)
+		{
+			nbpieces++;
+			if (nbpieces == 1) 
+				space = 0;
+		}
+		else if (board[i+counti][j+countj] == 0)
+			space++;
+		else if (space == 2 || (board[i+counti][j+countj] != player && board[i+counti][j+countj] != 0))
+			break;
+		countj++;
+	}
+
+	space = 0;
+	counti = 0;
+	countj = 0;
+
+	while ((i+counti > 0 && i+counti <= 18 && j+countj > 0 && j+countj <= 18) && countj > -3 && counti > -3 || (board[i+counti][j+countj] == player && j+countj > 0 && i+counti > 0))
+	{
+		counti--;
+		countj--;
+	}
+	while((i+counti >= 0 && i+counti <= 18 && j+countj >= 0 && j+countj <= 18))
+	{
+		if (board[i+counti][j+countj] == player)
+		{
+			nbpieces++;
+			if (nbpieces == 1) 
+				space = 0;
+		}
+		else if (board[i+counti][j+countj] == 0)
+			space++;
+		else if (space == 2 || (board[i+counti][j+countj] != player && board[i+counti][j+countj] != 0))
+			break;
+		countj++;
+		counti++;
+	}
+
+	space = 0;
+	counti = 0;
+	countj = 0;
+
+	while ((i+counti > 0 && i+counti < 18 && j+countj > 0 && j+countj <= 18) && countj > -3 && counti < 3 || (board[i+counti][j+countj] == player && j+countj > 0 && i+counti < 18))
+	{
+		counti++;
+		countj--;
+	}
+	while((i+counti >= 0 && i+counti <= 18 && j+countj >= 0 && j+countj <= 18))
+	{
+		if (board[i+counti][j+countj] == player)
+		{
+			nbpieces++;
+			if (nbpieces == 1) 
+				space = 0;
+		}
+		else if (board[i+counti][j+countj] == 0)
+			space++;
+		else if (space == 2 || (board[i+counti][j+countj] != player && board[i+counti][j+countj] != 0))
+			break;
+		countj++;
+		counti--;
+	}
+	return(nbpieces);
+}
+
 function	heuristique(i, j, player, win1, win2, prio)
 {
 	var	score = 0;
-
+	var nb = 0;
+	
 	if (win1)
 	{
 		if (current_player == player)
@@ -18,6 +116,8 @@ function	heuristique(i, j, player, win1, win2, prio)
 		else
 			score -= 50000;
 	}
+	nb = checkNbPieces(i, j, player);
+	score += nb * 10;
 	nbHeuristique++;
 	if (player == current_player)
 		return score;
